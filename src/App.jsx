da1723fa128106f1e562ab2a8effb4fc85816c32
dan <dis.131998@gmail.com>
2025-11-14 14:11:29 +0200

    add: mediabanner component and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
 import Banner from './components/Banner'
 import Nav from './components/Nav'
+import Media from './components/Media'
+
 function App() {
   return (
     <>
     <Banner/>
     <Nav/>
+    <Media/>
     </>
   )
 }
---
+++ src/components/Media.jsx
@@ -0,0 +1,10 @@
+import styles from "../styles/Media.module.css"
+function Media() {
+    return(<div className={styles.media}>
+        <div className={styles.wrapper}>
+            <h1>Shopping Cart</h1>
+            <h2>Beyond Expectations</h2>
+        </div>
+    </div>)
+}
+export default Media
